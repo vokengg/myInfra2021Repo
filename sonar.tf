26acc3ce8a918f183a41a2157074b9c0579e488e
Ubuntu <you@example.com>
2023-02-22 02:23:38 +0000

    sonar

--- sonar.tf
+++ sonar.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0ee23bfc74a881de5"
+      ami           = "ami-00eeedc4036573771"
       key_name = var.key_name
       instance_type = "t2.micro"
       vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
